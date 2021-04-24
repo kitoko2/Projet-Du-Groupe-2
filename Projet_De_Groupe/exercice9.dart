ed48b611f24d777df573c7fb06ee146f0714a12f
kitoko2 <81537647+kitoko2@users.noreply.github.com>
2021-04-24 17:06:43 +0000

    Update exercice9.dart

--- Projet_De_Groupe/exercice9.dart
+++ Projet_De_Groupe/exercice9.dart
@@ -9,4 +9,5 @@ void main() {
     print("entrer un nombre compris entre 1 et 3");
     nbr = double.parse(stdin.readLineSync()!);
   } while (nbr < 1 || nbr > 3);
+  // faire ... tant que la condition est vrai(le nombre n"est pas compris entre 1 et 3)
 }
